2291acd4c13e8e20d4f6bf720d1ef7b06f4779c3
Utkarsh Yadav <utkarsh.yadav5@gmail.com>
2018-11-28 16:24:50 +0530

    Password textbox added along with styling

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,20 +2,29 @@ import React, {Component} from 'react';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { text: ''};
+    state = { email: '', password: ''};
 
     render() {
         return (
             <Card>
                 <CardSection>
                     <Input 
-                        value={this.state.text}
-                        onChangeText={text => this.setState({ text })}
-                        style={{ height:20, width: 100 }}
+                        placeholder="user@gmail.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
                     />
                 </CardSection>
 
-                <CardSection />
+                <CardSection>
+                    <Input
+                        secureTextEntry
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
 
                 <CardSection>
                     <Button>
